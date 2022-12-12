0132926cd918b51e85a066debda7b2989d9b0952
Bjørn Arild Kristiansen <bjorn.arild.kristiansen@finn.no>
2022-12-12 18:02:05 +0100

    Day7

--- Common.scala
+++ Common.scala
@@ -1,28 +1,30 @@
+//> using scala "3.2.1"
+
 def readData(dayNo: Int) = scala.io.Source.fromFile(s"data/day$dayNo.txt").getLines().toList
 
 trait Solver[A] {
-  val decode: String => A
-  def dayNo: Int
-  def realData = readData(dayNo).map(decode)
-  def solve1(input: List[A]): Int
-  def solve2(input: List[A]): Int
+   val decode: String => A
+   def dayNo: Int
+   def realData = readData(dayNo).map(decode)
+   def solve1(input: List[A]): Int
+   def solve2(input: List[A]): Int
 }
 
 trait Solver2 {
-  type IN
-  type OUT = Int
-  def parse(s: String): IN
-  def dayNo: Int
-  def realData: List[IN] = readData(dayNo).map(parse)
-  def solve1(input: List[IN]): OUT
-  def solve2(input: List[IN]): OUT
-  def solve1Raw(input: List[String]): OUT = solve1(input.map(parse))
-  def solve2Raw(input: List[String]): OUT = solve2(input.map(parse))
+   type IN
+   type OUT = Int
+   def parse(s: String): IN
+   def dayNo: Int
+   def realData: List[IN] = readData(dayNo).map(parse)
+   def solve1(input: List[IN]): OUT
+   def solve2(input: List[IN]): OUT
+   def solve1Raw(input: List[String]): OUT = solve1(input.map(parse))
+   def solve2Raw(input: List[String]): OUT = solve2(input.map(parse))
 
 }
 
 trait StringSolver extends Solver2 {
-  override type IN = String
-  override def parse(s: String): String = s
-  override def realData                 = readData(dayNo)
+   override type IN = String
+   override def parse(s: String): String = s
+   override def realData                 = readData(dayNo)
 }
--- Day7.scala
+++ Day7.scala
@@ -1,5 +1,37 @@
-object Day7 {
-   case class Dir(subdirs: List[Dir], size: Int)
+object Day7 extends Solver2 {
 
+   opaque type DirId = String
+
+   val rootId: DirId = "/"
+
+   case class Dir(id: DirId, subdirs: List[Dir], size: Int)
+
+   enum Action {
+      case ChangeDirDown(id: DirId)
+      case ChangeDirUp
+      case ChangeDirToRoot
+      case ListFiles
+   }
    
+   import Action.*
+
+   type CdAction = ChangeDirDown | ChangeDirUp.type | ChangeDirToRoot.type
+
+   enum Output {
+      case DirOutput(dirId: DirId)
+      case FileOutput(size: Int)
+   }
+
+   type IN = Action | Output
+
+   def parseCd(cs: List[Char]): CdAction = cs match {
+      case '/' :: Nil => ChangeDirToRoot
+      case '.' :: '.' :: Nil => ChangeDirUp
+      case id => ChangeDirDown(id)
+   }
+
+   def parse(s: String): IN => s.toCharArray.toList match {
+      case 'c' :: 'd' :: ' ' :: rest => parseCd(rest)
+      case _ => ???
+   }
 }
